6466c49de102446f541b7128953c38e3ed584dcb
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2017-03-26 21:57:38 +0300

    [Add] pelicanconf.py decoration

--- pelicanconf.py
+++ pelicanconf.py
@@ -11,17 +11,23 @@ http://docs.getpelican.com/en/stable/settings.html#logging
 """
 import sys
 
+# ****************************************************************************
+# *                                  General                                 *
+# ****************************************************************************
+
 # Для того чтобы Pelican видел плагины из этой папки
 sys.path.append('plugins')
-
 
 AUTHOR = 'Саша Черных'
 SITENAME = 'Поиск Кристиниты'
 SITEURL = 'http://kristinita.ru'
 
-# Lang and timezone
 TIMEZONE = 'Europe/Moscow'
 DEFAULT_LANG = 'en'
+
+# ****************************************************************************
+# *                                   Feed                                   *
+# ****************************************************************************
 
 # Feed generation, perhaps, not desired when developing
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
@@ -34,42 +40,35 @@ AUTHOR_FEED_RSS = None
 # LINKS = (('Pelican', 'http://getpelican.com/'),
 #          ('Python.org', 'http://python.org/'),
 #          ('Jinja2', 'http://jinja.pocoo.org/'),
-#          ('You can modify those links in your config file', '#'),)
+#          ('You can edit those links in your config file', '#'),)
 
 # Social widget
 # SOCIAL = (('You can add links in your config file', '#'),
 #           ('Another social link', '#'),)
 
-DEFAULT_PAGINATION = False
-
-# Uncomment following line if you want document-relative URLs when developing
-RELATIVE_URLS = True
+# ****************************************************************************
+# *                                Generation                                *
+# ****************************************************************************
 
 # Перед генерацией стираем старый вариант сайта (кроме информации о
 # репозитории)
 DELETE_OUTPUT_DIRECTORY = True
+# Do not touch in generation process
 OUTPUT_RETENTION = [".git"]
-
-# Theme
-THEME = 'themes/sashapelican'
-
-PAGE_URL = '{slug}.html'
-PAGE_SAVE_AS = PAGE_URL
-CATEGORY_URL = 'category/{slug}.html'
-CATEGORY_SAVE_AS = CATEGORY_URL
-TAG_URL = 'tag/{slug}.html'
-TAG_SAVE_AS = TAG_URL
-TAGS_SAVE_AS = 'tag/alltags.html'
 
 LOAD_CONTENT_CACHE = False
 
-TYPOGRIFY = True
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
 
+# Original format without rendering. If true, md rendering to md,
+# not md → html
+OUTPUT_SOURCES = False
 
-# Путь, где пишется контент сайта
-PATH = 'content'
+# ****************************************************************************
+# *                                  Plugins                                 *
+# ****************************************************************************
 
-# Plugins
 PLUGIN_PATHS = ['pelican-plugins']
 PLUGINS = ['pagefixer', 'pelican_javascript', 'sitemap']
 
@@ -89,7 +88,14 @@ SITEMAP = {
     }
 }
 
-OUTPUT_SOURCES = False
+# ****************************************************************************
+# *                                   Paths                                  *
+# ****************************************************************************
+
+# Путь, где пишется контент сайта
+PATH = 'content'
+
+
 # Путь к pages
 # Don't set “PAGE_PATHS = ['']”! See
 # https://github.com/getpelican/pelican/issues/2123
@@ -97,7 +103,7 @@ PAGE_PATHS = ['Gingerinas', 'Giologica', 'Life-hacks', 'Sasha-Black',
               'Smert-svobode', 'Sublime-Text']
 # Путь к articles
 # [Хак] Чтобы исключить articles, подставил несуществующую папку.
-# «ARTICLE_PATHS = None» — critical error
+# «ARTICLE_PATHS = None» — critical error, integer parameter
 ARTICLE_PATHS = ['None']
 # Не модифицируемые папки и файлы
 # [Хак] Почему-то регистр имеет значение. 404.md и 404.html уже не работает.
@@ -113,6 +119,26 @@ EXTRA_PATH_METADATA = {
     'extra/verifyforzoho.HTML': {'path': 'zohoverify/verifyforzoho.html'},
     'favicons/favicon.ico': {'path': 'favicon.ico'},
 }
+
+PAGE_URL = '{slug}.html'
+PAGE_SAVE_AS = PAGE_URL
+CATEGORY_URL = 'category/{slug}.html'
+CATEGORY_SAVE_AS = CATEGORY_URL
+TAG_URL = 'tag/{slug}.html'
+TAG_SAVE_AS = TAG_URL
+TAGS_SAVE_AS = 'tag/alltags.html'
+
+# ****************************************************************************
+# *                                   Other                                  *
+# ****************************************************************************
+
+# Theme
+THEME = 'themes/sashapelican'
+
+# Pagination — division of articles
+DEFAULT_PAGINATION = False
+
+TYPOGRIFY = True
 
 # Disable logging of empty alt attribute, needs “import logging” module
 LOG_FILTER = [(logging.WARN, 'Empty alt attribute for image %s in %s')]
