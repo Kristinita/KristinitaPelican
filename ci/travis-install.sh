a2499d938262ddaad55c4ba29976e1fca5f7933a
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2018-04-08 19:32:34 +0300

    ci(travis): Move HTML Tidy Ubuntu build to parallel tasks
    https://codeyarns.com/2016/06/06/how-to-build-and-install-html-tidy/ https://unix.stackexchange.com/a/427419/237999

--- ci/travis-install.sh
+++ ci/travis-install.sh
@@ -4,5 +4,10 @@
 # https://unix.stackexchange.com/questions/427410#comment771128_427415
 # [NOTE] Grunt tasks needs grunt-cli globally:
 # https://stackoverflow.com/a/21133609/5951529
+# [WARNING] Don't paste any comments after “parallel :::”, parallel will not work:
+# https://travis-ci.org/Kristinita/SashaScrutinizer/builds/363772800
+# [WARNING] apt-get HTML Tidy version outdated, build HTML tidy on Ubuntu from sources:
+# https://codeyarns.com/2016/06/06/how-to-build-and-install-html-tidy/
 parallel ::: 'pip install --upgrade pip && pip install pipenv && pipenv install --dev' \
-             'npm install --global npm && npm install -g grunt-cli && npm install'
+             'npm install --global npm && npm install -g grunt-cli && npm install' \
+             'git clone https://github.com/htacg/tidy-html5.git && cd tidy-html5 && cd build/cmake && cmake ../.. && cmake --build . --config Release && sudo make install'
